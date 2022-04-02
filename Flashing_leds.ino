0428cb0d15ca051b031e206c74153fa9fe3cc641
Liam-07J <94325296+Liam-07J@users.noreply.github.com>
2022-04-02 22:29:32 +0100

    Added Buzzer
    Added Buzzer Randomly turning on and off!

--- Flashing_leds.ino
+++ Flashing_leds.ino
@@ -1,16 +1,28 @@
 void setup() {
   // put your setup code here, to run once:
-  pinMode(13, OUTPUT);
-  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);    //  pin 13 is the 1st led
+  pinMode(12, OUTPUT);    //  pin 12 is the 2nd led
+  pinMode(11, OUTPUT);    //  pin 11 is the buzzer pin
+  Serial.begin(9600);
 }
 
 void loop() {
+  // Generates random Numbers
   long randomSpeed = random(40,500);
   long randomSpeed2 = random(50,700);
+  // Switches LEDs off and on
   digitalWrite(13, LOW);
   digitalWrite(12, HIGH);
   delay(randomSpeed);
+  // Switches LEDs off and on
   digitalWrite(12, LOW);
   digitalWrite(13, HIGH);
   delay(randomSpeed2);
+  // randomly activates buzzer
+  if (random(1,10) == 1){
+   analogWrite(11, random(1,255));
+   delay(50);
+   analogWrite(11, 0);
+  }
+  delay(randomSpeed);
 }
